964b5717b45d6613d5c3da1b8c90e6a2890d3b95
Garrett Summerfield <garrettsummerfi3ld@gmail.com>
2019-05-09 00:04:59 -0500

    Motors implemented
    This is not tested

--- doorknob.py
+++ doorknob.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
 
-from gpiozero import LED, Button
+from gpiozero import LED, Button, Motor
+from time import sleep
 
+motor = Motor(forward=4, backward=14)
 led = LED(17)
 button = Button(2)
 
 while True:
     if button.is_pressed:
         led.on()
-        print("LED On!")
+        motor.forward()
+        print("Motor starting!")
     else:
         led.off()
-        print("LED Off")
+        motor.backward()
+        print("Motor stopped!")
