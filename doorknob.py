85f26e54dd0022be7353895d59a1a55da8e70852
Garrett Summerfield <garrettsummerfi3ld@gmail.com>
2019-05-13 12:27:52 -0500

    DO NOT MERGE - Adding logging, OS Detection

--- doorknob.py
+++ doorknob.py
@@ -2,16 +2,27 @@
 
 # Imports
 import logging
-from gpiozero import LED, Button, Motor
+import platform
+import sys
+#from gpiozero import LED, Button, Motor
 from datetime import datetime
 from time import sleep
 
+# Vars
+#motor = Motor(forward=4, backward=14)
+#led = LED(17)
+#button = Button(2)
+filelocation = './log/doorknob.log'
+
+# Create Dirs and files
+def createDirs():
+    if not os.path.exists('./log'):
+        os.path.join(filelocation)
+    else:
+        return
+
 # Logging
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
-                    datefmt='%Y-%m-%d %H:%M:%S',
-                    filename='/temp/doorknob.log',
-                    filemode='a')
+logging.basicConfig(level=logging.DEBUG,format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',datefmt='%Y-%m-%d %H:%M:%S',filename=filelocation)
 # define a Handler which writes INFO messages or higher to the sys.stderr
 console = logging.StreamHandler()
 console.setLevel(logging.INFO)
@@ -21,26 +32,83 @@ formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
 console.setFormatter(formatter)
 # add the handler to the root logger
 logging.getLogger('').addHandler(console)
-
+# Logging vars
 mainLogger = logging.getlogger('doorknob.main')
+osLogger = logging.getLogger('doorknob.os')
 
-# Vars
-motor = Motor(forward=4, backward=14)
-led = LED(17)
-button = Button(2)
+# Checks for the right OS Host
+def osProbe():
+    osLogger.info("Checking OS...")
+    warnFlag = 0
+    # Supported platforms to run the script
+    supportedPlatforms = {
+        'linux1' : 'Linux',
+        'linux2' : 'Linux',
+        'linux' : 'Linux'
+    }
+    # Unsupported platforms to run the script, this is not recommended to run
+    unsupportedPlatforms = {
+        'win32' : 'Windows',
+        'win64' : 'Windows',
+        'darwin' : 'macOS'
+    }
+    try:
+        # Pull information from system
+        osVersion = str(platform.version())
+        osRelease = str(platform.release())
+        osSystem = str(platform.system())
+        osPlatform = str(sys.platform())
+        # If system is supported
+        if osPlatform in supportedPlatforms:
+            osLogger.info("Detected OS is supported!")
+            osLogger.debug("==[ OS INFO ]==")
+            osLogger.debug("OS VERSION: " + osVersion)
+            osLogger.debug("OS RELEASE: " + osRelease)
+            osLogger.debug("OS VERSION: " + osVersion)
+            osLogger.debug("OS SYSTEM: " + osSystem)
+            osLogger.debug("==[ OS INFO ]==")
+            return
+        # If system is not supported
+        elif osPlatform in unsupportedPlatforms:
+            warnFlag = 1
+            osLogger.warning("Detected OS is **NOT** SUPPORTED, I AM NOT RESPONSIBLE FOR CATESTROPHIC FAILURE")
+            osLogger.debug("==[ OS INFO ]==")
+            osLogger.debug("OS VERSION: " + osVersion)
+            osLogger.debug("OS RELEASE: " + osRelease)
+            osLogger.debug("OS VERSION: " + osVersion)
+            osLogger.debug("OS SYSTEM: " + osSystem)
+            osLogger.debug("==[ OS INFO ]==")
+            # Run through a check to see if user is okay with the script being broken
+            while warnFlag == 1:
+                osLogger.warning("ARE YOU SURE YOU WANT TO CONTINUE? [Y/n]")
+                confirmWarn = str(input(""))
+                # Run script
+                if confirmWarn == 'Y' & confirmWarn == 'y':
+                    osLogger.info("Continuing on...")
+                    return
+                # Stop script
+                else:
+                    osLogger.info("Closing application...")
+                    break
+            return
+    except:
+        osLogger.error("ERROR! CHECK STACKTRACE! LOGFILE IN")
 
 # main class
 def main():
+    # Setting up logs and other dependencies
+    createDirs()
+    # Check OS
+    osProbe()
     try:
-        testCondition = input("input a condition: ")
-
-        if testCondition == yes:
+        testCondition = str(input("input a condition: "))
+        if testCondition == 'yes':
             led.on()
             motor.forward()
             mainLogger.info("Motor starting!")
-        if testCondition == no:
+        elif testCondition == 'no':
             led.off()
             motor.off()
             mainLogger.info("Motor stopped!")
     except:
-        mainLogger.error("ERROR! CHECK STACKTRACE!")
+        mainLogger.error("ERROR! CHECK STACKTRACE! LOGFILE IN")
