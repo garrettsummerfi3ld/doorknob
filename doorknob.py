af3c5695a90748a9c43471044a1cb884f54b36e2
Garrett Summerfield <garrettsummerfi3ld@gmail.com>
2019-05-14 23:03:57 -0500

    Several fixes and refactoring

--- doorknob.py
+++ doorknob.py
@@ -5,57 +5,55 @@ import logging
 import platform
 import sys
 import os
-#from gpiozero import LED, Button, Motor
-from datetime import datetime
+from gpiozero import LED, Button, Motor
 from time import sleep
 
 # Vars
-#motor = Motor(forward=4, backward=14)
-#led = LED(17)
-#button = Button(2)
+motor = Motor(forward=4, backward=14)
+led = LED(17)
+button = Button(2)
 fileLocation = 'doorknob.log'
+
+# Logging
+logging.basicConfig(level=logging.DEBUG,format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',datefmt='%Y-%m-%d %H:%M:%S',filename=fileLocation)
+
+# define a Handler which writes INFO messages or higher to the sys.stderr
+console = logging.StreamHandler()
+
+# set a format which is simpler for console use
+formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+
+# tell the handler to use this format
+console.setFormatter(formatter)
+
+# add the handler to the root logger
+logging.getLogger('').addHandler(console)
+
 # Create Dirs and files
-def createDirs():
+def create_dirs():
     if not os.path.exists('./log'):
         os.path.join(fileLocation)
         logFile = open(fileLocation,"w")
     else:
         return
 
-# Logging
-logging.basicConfig(level=logging.DEBUG,format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',datefmt='%Y-%m-%d %H:%M:%S',filename=fileLocation)
-# define a Handler which writes INFO messages or higher to the sys.stderr
-console = logging.StreamHandler()
-console.setLevel(logging.INFO)
-# set a format which is simpler for console use
-formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
-# tell the handler to use this format
-console.setFormatter(formatter)
-# add the handler to the root logger
-logging.getLogger('').addHandler(console)
-
-# Checks for the right OS Host
-def osProbe():
+# Checks for the right OS platform
+def os_probe():
     logging.info("Checking OS...")
     warnFlag = 0
+    
     # Supported platforms to run the script
-    supportedPlatforms = {
-        'linux1' : 'Linux',
-        'linux2' : 'Linux',
-        'linux' : 'Linux'
-    }
+    supportedPlatforms = ['linux1','linux2','linux']
+    
     # Unsupported platforms to run the script, this is not recommended to run
-    unsupportedPlatforms = {
-        'win32' : 'Windows',
-        'win64' : 'Windows',
-        'darwin' : 'macOS'
-    }
+    unsupportedPlatforms = ['win32','win64','Windows','darwin','macOS']
     try:
         # Pull information from system
         osVersion = platform.version()
         osRelease = platform.release()
         osSystem = platform.system()
         osPlatform = platform.system()
+        
         # If system is supported
         if osPlatform in supportedPlatforms:
             logging.info("Detected OS is supported!")
@@ -66,6 +64,7 @@ def osProbe():
             logging.debug("OS SYSTEM: " + osSystem)
             logging.debug("==[ OS INFO ]==")
             return
+        
         # If system is not supported
         elif osPlatform in unsupportedPlatforms:
             warnFlag = 1
@@ -76,28 +75,39 @@ def osProbe():
             logging.debug("OS VERSION: " + osVersion)
             logging.debug("OS SYSTEM: " + osSystem)
             logging.debug("==[ OS INFO ]==")
+           
             # Run through a check to see if user is okay with the script being broken
             while warnFlag == 1:
                 logging.warning("ARE YOU SURE YOU WANT TO CONTINUE? [Y/n]")
                 confirmWarn = str(input(""))
+                
                 # Run script
-                if confirmWarn == 'Y' & confirmWarn == 'y':
+                if confirmWarn == 'Y' or confirmWarn == 'y':
                     logging.info("Continuing on...")
                     return
+                
                 # Stop script
-                else:
+                elif confirmWarn == 'N' or confirmWarn == 'n':
                     logging.info("Closing application...")
-                    break
+                    exit()
+                else:
+                    logging.info("")
             return
     except:
         logging.error("ERROR! CHECK STACKTRACE! LOGFILE IN")
 
 # main class
 def main():
+    print("     _                  _                _")
+    print("  __| | ___   ___  _ __| | ___ __   ___ | |__")
+    print(" / _` |/ _ \\ / _ \\| '__| |/ / '_ \\ / _ \\| '_ \\")
+    print("| (_| | (_) | (_) | |  |   <| | | | (_) | |_) |")
+    print(" \\__,_|\\___/ \\___/|_|  |_|\\_\\_| |_|\\___/|_.__/")
+    print("by doorX // version 0.1.3 alpha")
     # Setting up logs and other dependencies
-    createDirs()
+    create_dirs()
     # Check OS
-    osProbe()
+    os_probe()
     try:
         testCondition = str(input("input a condition: "))
         if testCondition == 'yes':
