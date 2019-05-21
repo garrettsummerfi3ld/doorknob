a6b0addaaea924d6eb64db3193c2bcb9b9648ae4
Garrett Summerfield <garrettsummerfi3ld@gmail.com>
2019-05-20 23:36:48 -0500

    Adding sleep statements
    Would break door

--- public/python/door_close.py
+++ public/python/door_close.py
@@ -1,6 +1,8 @@
 # Edit line 6 to match your chosen GPIO pin
 
 import RPi.GPIO as GPIO
+from time import sleep
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(26, GPIO.OUT)
+sleep(5)
--- public/python/door_open.py
+++ public/python/door_open.py
@@ -1,7 +1,9 @@
 # Edit line 6 to match your chosen GPIO pin
 
 import RPi.GPIO as GPIO
+from time import sleep
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(20, GPIO.IN)
+sleep(5)
 GPIO.cleanup()
