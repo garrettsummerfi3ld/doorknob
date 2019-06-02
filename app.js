72a98e76c60286510fcab6f8ea6a88d76a719591
Garrett Summerfield <garrettsummerfi3ld@gmail.com>
2019-06-01 20:31:52 -0500

    Added ESLint for linting and code style enforcement
    Will be using Google's default rules for code style as a set default

--- app.js
+++ app.js
@@ -1,151 +1,173 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const PythonShell = require("python-shell");
-const fs = require("fs");
-const express = require("express");
-const bodyParser = require("body-parser");
-const path = require("path");
+const PythonShell = require('python-shell');
+const fs = require('fs');
+const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
 // Switch states held in memory
 const switches = [];
 
 // Read state from saveState.json, populate switches array
-var readableStream = fs.createReadStream("saveState.json");
-var data = "";
+const readableStream = fs.createReadStream('saveState.json');
+let data = '';
 
-readableStream.on("data", function (chunk) {
-    data += chunk;
+readableStream.on('data', function(chunk) {
+  data += chunk;
 });
 
-readableStream.on("end", function () {
-    var parsed = JSON.parse(data);
+readableStream.on('end', function() {
+  const parsed = JSON.parse(data);
 
-    for (i = 0; i < parsed.switches.length; i++) {
-        switches.push(new Switch(parsed.switches[i]));
-    }
+  for (i = 0; i < parsed.switches.length; i++) {
+    switches.push(new Switch(parsed.switches[i]));
+  }
 });
 
-// Switch Model
-// Expects an object:{
-// id:"sw" + number,
-// state: "on" or "off",
-// name: any name you want to display. Defaults to "switch"
-// }
+/**
+ * Switches for the application
+ * Expects an object
+ * id:"sw" + number
+ * state: "on" or "off"
+ * name: any name you want to display. Defaults to "switch"
+ */
 class Switch {
-    constructor(switchValues) {
-        this.id = switchValues.id || "door";
-        this.state = switchValues.state || "off";
-        this.name = switchValues.name || "doorx";
-        this.toggle = function () {
-            if (this.state === "on") {
-                this.setState("open");
-            }
-            else {
-                this.setState("off");
-            }
-        };
-        this.setState = function (state) {
-            var str = state === "on" ? onString(this.id[2]) : offString(this.id[2]);
-            PythonShell.run(str, function (err) {
-                if (!process.env.DEV) {
-                    if (err)
-                        throw err;
-                }
-            });
-            this.state = state;
-        };
-        // Invokes setState on init to set the switch to its last recalled state.
-        this.setState(this.state);
-    }
-}
-
-// needed due to a quirk with PythonShell
-function onString(number) {
-    return "./public/python/door_open.py";
-}
-
-function offString(number) {
-    return "./public/python/door_close.py";
-}
-
-// Switch Lookup
-function getSwitch(string) {
-    return switches.filter(function (element) {
-        return element.id === string;
-    })[0];
-}
-
-// Updates saveState.json
-function saveState() {
-    var formattedState = {
-        switches: switches
+  /**
+   * Constructor for the switch
+   * @param {*} switchValues
+   * @param {id} sets switch values for the id
+   */
+  constructor(switchValues) {
+    this.id = switchValues.id || 'door';
+    this.state = switchValues.state || 'off';
+    this.name = switchValues.name || 'doorx';
+    this.toggle = function() {
+      if (this.state === 'on') {
+        this.setState('open');
+      } else {
+        this.setState('off');
+      }
     };
-    fs.writeFile("./saveState.json", JSON.stringify(formattedState), function (
-        err
-    ) {
-        if (err) {
-            console.error("ERROR! CHECK STACTRACE!")
-            console.error(err);
-        } else {
-            let date = new Date();
-            console.log(`
+    this.setState = function(state) {
+      const str = state === 'on' ? onString(this.id[2]) : offString(this.id[2]);
+      PythonShell.run(str, function(err) {
+        if (!process.env.DEV) {
+          if (err) {
+            throw err;
+          }
+        }
+      });
+      this.state = state;
+    };
+    // Invokes setState on init to set the switch to its last recalled state.
+    this.setState(this.state);
+  }
+}
+
+/**
+ * Python script that causes  a door to open
+ * @param {number} number
+ * @return {void} door open function
+ */
+function onString(number) {
+  return './public/python/door_open.py';
+}
+
+/**
+ * Python script that causes  a door to open
+ * @param {number} number
+ * @return {void} door closing function
+ */
+function offString(number) {
+  return './public/python/door_close.py';
+}
+
+/**
+ *
+ * @param {getSwitch} string
+ * @return {switches} function of the element after filtering out
+ * @return {element.id} returns string of the id for the parameters
+ */
+function getSwitch(string) {
+  return switches.filter(function(element) {
+    return element.id === string;
+  })[0];
+}
+
+/**
+ * Updates saveState.json
+ */
+function saveState() {
+  const formattedState = {
+    switches: switches,
+  };
+  fs.writeFile('./saveState.json', JSON.stringify(formattedState), function(
+      err
+  ) {
+    if (err) {
+      console.error('ERROR! CHECK STACKTRACE!');
+      console.error(err);
+    } else {
+      const date = new Date();
+      console.log(`
 ${date.toLocaleDateString()} ${date.toLocaleTimeString()} State has been updated
 New state: ${JSON.stringify(formattedState)}
 `);
-        }
-    });
+    }
+  });
 }
 
-//Server Configuration
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(__dirname + "/public"));
+// Server Configuration
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
 
-// If you have a frontend, drop it in the Public folder with an entry point of index.html
-app.get("/", function (req, res) {
-    res.sendFile("index");
+/**
+ * If you have a frontend, drop it in the Public folder with an entry point of
+ * index.html
+ */
+app.get('/', function(req, res) {
+  res.sendFile('index');
 });
 
 // Switch Routes for API
-app.get("/api/switches", function (req, res) {
-    res.send(switches);
+app.get('/api/switches', function(req, res) {
+  res.send(switches);
 });
 
-app.get("/api/switches/:id", function (req, res) {
-    var found = getSwitch(req.params.id);
-    res.json(found);
+app.get('/api/switches/:id', function(req, res) {
+  const found = getSwitch(req.params.id);
+  res.json(found);
 });
 
-app.post("/api/switches/:id", function (req, res) {
-    // For now, uses a simple password query in the url string.
-    // Example: POST to localhost:8000/API/switches/sw1?password=test
-    if (req.query.password === process.env.PASS) {
-        try {
-            var foundSwitch = getSwitch(req.params.id);
-            
-            // Optional On / Off command. If not included, defaults to a toggle.
-            if (!(req.query.command === "open" || req.query.command === "close")) {
-                foundSwitch.toggle();
-            } else {
-                foundSwitch.setState(req.query.command);
-            }
-    
-            saveState();
-            console.log("postSwitch " + JSON.stringify(foundSwitch));
-            res.json(foundSwitch);
-            
-        } catch (error) {
-            console.error("ERROR! CHECK STACKTRACE!")
-            console.error(error)
-        }
+app.post('/api/switches/:id', function(req, res) {
+  // For now, uses a simple password query in the url string.
+  // Example: POST to localhost:8000/API/switches/sw1?password=test
+  if (req.query.password === process.env.PASS) {
+    try {
+      const foundSwitch = getSwitch(req.params.id);
 
-    } else {
-        console.log("ERROR! AUTHENTICATION FAILURE");
-        res.send("CHECK AUTHENTICATION METHODS");
+      // Optional On / Off command. If not included, defaults to a toggle.
+      if (!(req.query.command === 'open' || req.query.command === 'close')) {
+        foundSwitch.toggle();
+      } else {
+        foundSwitch.setState(req.query.command);
+      }
+
+      saveState();
+      console.log('postSwitch ' + JSON.stringify(foundSwitch));
+      res.json(foundSwitch);
+    } catch (error) {
+      console.error('ERROR! CHECK STACKTRACE!');
+      console.error(error);
     }
+  } else {
+    console.log('ERROR! AUTHENTICATION FAILURE');
+    res.send('CHECK AUTHENTICATION METHODS');
+  }
 });
 
-app.listen(process.env.PORT, function () {
-    console.log("Doorknob service started!")
-    console.log("Listening on port " + process.env.PORT);
+app.listen(process.env.PORT, function() {
+  console.log('Doorknob service started!');
+  console.log('Listening on port ' + process.env.PORT);
 });
