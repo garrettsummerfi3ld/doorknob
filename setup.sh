0e3f354981699be721d412e9e7b40dc79919af9f
Garrett Summerfield <garrettsummerfi3ld@gmail.com>
2019-08-22 23:41:57 -0500

    Starting setup script for requirements

--- setup.sh
+++ setup.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-echo("Placeholder")
+
+# Checking for python3 install
+check_python3() {
+    #using dpkg to check if python is installed
+    local python3Status=$(dpkg-query -W python3 | grep "install ok installed" &> /dev/null; echo $?)
+}
+
+    if $python3Status == 0
+        then
+            echo "Python3 is installed!"
+        else
+            echo "Python3 is not installed!"
+
+echo "Setup for doorknob"
+check_python3
